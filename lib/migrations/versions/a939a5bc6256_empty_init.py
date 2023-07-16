48fca408b99f9b2690564c1b192bcc2725d2e4e2
Jonathan Grabowski <jon.grabowski@gmail.com>
2023-07-16 18:36:05 -0400

    Lab completed

---
+++ lib/migrations/versions/a939a5bc6256_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: a939a5bc6256
+Revises: 
+Create Date: 2023-07-16 18:15:58.340483
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a939a5bc6256'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
